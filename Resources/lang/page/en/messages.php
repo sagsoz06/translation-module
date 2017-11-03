a16156d6a0830c30f1edecd13e3a7ad41a6b8fda
aniliso <sagsoz06@gmail.com>
2017-11-03 23:43:26 +0300

    Added Page translations

--- Resources/lang/page/en/messages.php
+++ Resources/lang/page/en/messages.php
@@ -5,6 +5,7 @@ return [
     'page not found' => 'Page not found.',
     'page updated' => 'Page successfully updated.',
     'page deleted' => 'Page successfully deleted.',
+    'permission denied' => 'You do not have permission to view this page!',
 
     'template is required' => 'The template name is required.',
     'title is required' => 'The title is required.',
--- Resources/lang/page/tr/messages.php
+++ Resources/lang/page/tr/messages.php
@@ -1,15 +1,16 @@
 <?php
 
 return [
-    'page created' => 'Sayfa başarıyla oluşturuldu.',
-    'page not found' => 'Sayfa bulunamadı.',
-    'page updated' => 'Sayfa başarıyla güncellendi.',
-    'page deleted' => 'Sayfa başarıyla silindi.',
+    'page created'      => 'Sayfa başarıyla oluşturuldu.',
+    'page not found'    => 'Sayfa bulunamadı.',
+    'page updated'      => 'Sayfa başarıyla güncellendi.',
+    'page deleted'      => 'Sayfa başarıyla silindi.',
+    'permission denied' => 'Bu sayfayı görmek için yetkiniz yok!',
 
-    'template is required' => 'Template adı gerekli.',
-    'title is required' => 'Başlık doldurulmalı.',
-    'body is required' => 'İçerik doldurulmalı.',
+    'template is required'      => 'Template adı gerekli.',
+    'title is required'         => 'Başlık doldurulmalı.',
+    'body is required'          => 'İçerik doldurulmalı.',
     'only one homepage allowed' => 'Sadece bir anasayfa oluşturulabilir',
-    'is_home' => 'Anasayfa Silinemez',
-    'return homepage' => 'Anasayfaya geri dön'
+    'is_home'                   => 'Anasayfa Silinemez',
+    'return homepage'           => 'Anasayfaya geri dön'
 ];
